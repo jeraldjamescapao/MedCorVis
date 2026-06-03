4b0a7b3dd6f3d2f4ae967d09027914e04950d123
Jerald James Capao <2690162+jeraldjamescapao@users.noreply.github.com>
2026-06-04 01:52:53 +0200

    refactor(users-tests): remove Identity project reference from test project Context - Users module no longer depends on Identity - the test project still referenced Identity only for ApplicationUser.SelfRegisteredActor   in UserProfileServiceTestBase
    Changes
    - replace ApplicationUser.SelfRegisteredActor with "Self" in UserProfileServiceTestBase
    - remove Identity project reference from MedCorVis.Modules.Users.Tests.csproj

--- tests/MedCorVis.Modules.Users.Tests/Application/Services/UserProfileServiceTestBase.cs
+++ tests/MedCorVis.Modules.Users.Tests/Application/Services/UserProfileServiceTestBase.cs
@@ -2,7 +2,6 @@ namespace MedCorVis.Modules.Users.Tests.Application.Services;
 
 using Microsoft.Extensions.Logging.Abstractions;
 using MedCorVis.Common.UserProfiles;
-using MedCorVis.Modules.Identity.Domain.Users;
 using MedCorVis.Modules.Users.Application.Abstractions;
 using MedCorVis.Modules.Users.Application.Services;
 using MedCorVis.Modules.Users.Domain;
@@ -29,6 +28,6 @@ public abstract class UserProfileServiceTestBase
             firstName: "Jerald James Capao",
             lastName:  "Test",
             birthDate: new DateOnly(1988, 6, 27),
-            createdBy: ApplicationUser.SelfRegisteredActor);
+            createdBy: "Self");
     }
 }
